49d504c4027b28364c005648fb53225fe63a0a88
Kaif Rabi <30978437+KaifRabi@users.noreply.github.com>
2022-11-12 20:29:31 +0500

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,21 @@
   <div id="app" class="bg-black">
     <NavBar />
     <HeroSection />
+    <InfoCard />
   </div>
 </template>
 
 <script>
 import HeroSection from './components/HeroSection.vue'
 import NavBar from './components/NavBar.vue'
+import InfoCard from './components/InfoCard.vue'
 
 export default {
   name: 'App',
   components: {
     NavBar,
     HeroSection,
+    InfoCard
   }
 }
 </script>
